9ae32371fe5cc0c9c45be66577db9a2b37ee4516
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 10:54:06 -0700

    Creacion de Entidad Bitacora

---
+++ src/main/java/com/bitacora/service/entidad/Bitacora.java
@@ -0,0 +1,53 @@
+package com.bitacora.service.entidad;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "bitacora")
+public class Bitacora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String mensaje;
+
+    private Date fecha;
+    
+    public Bitacora(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
+
