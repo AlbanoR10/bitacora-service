bafa93340ac6b9a1e945bf503bfb0ef95f98d535
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 10:48:28 -0700

    Inicio del proyecto bitacora-service

---
+++ src/test/java/com/bitacora/service/BitacoraServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitacora.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
